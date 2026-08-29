19a2ae5689b6d8320991b458ae4dfd3a86199c26
nyokolodi <m.nyokolodi93@gmail.com>
2026-08-29 13:57:35 +0200

    Fix ESLint flat config shape

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,7 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals.js';
-export default defineConfig([...nextVitals,globalIgnores(['.next/**','node_modules/**'])]);
+
+export default defineConfig(
+  nextVitals,
+  { ignores: ['.next/**', 'node_modules/**'] }
+);
