3afb7709fce4de5d3e106de52d4339a9adb7cd94
nyokolodi <m.nyokolodi93@gmail.com>
2026-08-29 13:54:13 +0200

    Fix ESLint Next.js config import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,3 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals';
-export default defineConfig([...nextVitals, globalIgnores(['.next/**','node_modules/**'])]);
+import nextVitals from 'eslint-config-next/core-web-vitals.js';
+export default defineConfig([...nextVitals,globalIgnores(['.next/**','node_modules/**'])]);
